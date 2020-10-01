b6533287192ea22e86d6a4b258a080ded978fd2d
mirobid <mirobid_i@mail.ru>
2020-10-01 15:20:47 +0500

    Homework 2: Task 7 added

--- src/main/java/lesson1/java_hw2_nasirov_m.java
+++ src/main/java/lesson1/java_hw2_nasirov_m.java
@@ -53,7 +53,26 @@ public class java_hw2_nasirov_m {
     int[] arr6 = {6, 5, 4, 4, 5, 6};
     method(arr6);
 
+        //Задание 7
+        int[] arr7={1,2,3,4,5};
+        shiftArr(arr7,1);
+        System.out.println(Arrays.toString(arr7));
+
 }
+
+
+    public static int[] shiftArr(int[] arr, int n) {
+        int k=n%arr.length+arr.length;
+        for (int i=0;i<k;i++){
+            int tmp=arr[arr.length-1];
+            for (int j=arr.length-1;j>0;j--){
+                arr[j]=arr[j-1];
+            }
+            arr[0]=tmp;
+        }
+        return arr;
+    }
+
 
     public static void method(int[] arr) {
         int sum1 = 0;
