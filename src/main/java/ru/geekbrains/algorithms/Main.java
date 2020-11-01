aad4d5c2388472b9306ce2a4d20fb315be558fc7
mirobid <mirobid_i@mail.ru>
2020-11-02 02:44:16 +0500

    Lists

--- src/main/java/ru/geekbrains/algorithms/Main.java
+++ src/main/java/ru/geekbrains/algorithms/Main.java
@@ -1,55 +1,24 @@
 package ru.geekbrains.algorithms;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Iterator;
+import java.util.*;
+
+class StackList{
+    private LinkedList list;
+    public StackList() {list=new LinkedList();}
+    public void push(String name){list.add(name);}
+    public void pop(int x){list.remove(x);}
+    public boolean isEmpty(){return list.isEmpty();}
+    public void display(int x){list.get(x);}
+}
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-        int x = 3;
         int startTime;
         int finishTime;
         Random rand=new Random();
-        int key=rand.nextInt(4);
-
-        startTime=(int) System.nanoTime();
-        String[] arr=new String[4];
-        arr[0]="Zero";
-        arr[1]="One";
-        arr[2]="Two";
-        arr[3]="Three";
-        finishTime=(int) System.nanoTime();
-
-        System.out.println(Arrays.toString(arr));
-        System.out.println(finishTime-startTime);
-
-        startTime=(int) System.nanoTime();
-        ArrayList<String> mArray=new ArrayList<>();
-        mArray.add("Zero");
-        mArray.add("One");
-        mArray.add("Two");
-        mArray.add("Three");
-        finishTime=(int) System.nanoTime();
-
-        System.out.println(mArray);
-        System.out.println(finishTime-startTime);
-
-        //добавление
-        startTime=(int) System.nanoTime();
-        mArray.add(2,"Element");
-        System.out.println(mArray);
-
-        //удаление
-        mArray.remove(2);
-        System.out.println(mArray);
-
-        //получение элемента из списка
-        System.out.println(mArray.get(2));
-        finishTime=(int) System.nanoTime();
-        System.out.println(finishTime-startTime);
 
         //простой список
         LinkedList<String> listL=new LinkedList<>();
@@ -62,18 +31,80 @@ public class Main {
         listL.remove(1);
         System.out.println(listL.get(1));
 
-        //итератор
+
+        //Задача 4.1
         startTime=(int) System.nanoTime();
-        Iterator<String> iter=mArray.iterator();
-        while(iter.hasNext()){
-            System.out.println(iter.next());
-            iter.remove();
+        Stack<String> sList=new Stack<>();
+
+        sList.push("One");
+        sList.push("Two");
+        sList.push("Three");
+        sList.push("Four");
+
+        while(!sList.empty()){
+            System.out.println(sList);
+            sList.pop();
         }
-        System.out.println(mArray);
         finishTime=(int) System.nanoTime();
         System.out.println(finishTime-startTime);
+
+        //Задача 4.2
+        startTime=(int) System.nanoTime();
+        Queue<String> qList=new LinkedList<>();
+        
+        qList.add("One");
+        qList.add("Two");
+        qList.add("Three");
+
+        while(!qList.isEmpty()){
+            System.out.println(qList);
+            qList.poll();
+        }
+        finishTime=(int) System.nanoTime();
+        System.out.println(finishTime-startTime);
+
+        //Задача 4.3
+        startTime=(int) System.nanoTime();
+        Deque<String> dList=new LinkedList<>();
+
+        dList.add("One");
+        dList.add("Two");
+        dList.add("Three");
+        dList.addFirst("Zero");
+        dList.addLast("Last");
+
+        while (!dList.isEmpty()){
+            System.out.println(dList);
+            dList.poll();
+        }
+        finishTime=(int) System.nanoTime();
+        System.out.println(finishTime-startTime);
+
+        //Задача 4.4
+        startTime=(int) System.nanoTime();
+        PriorityQueue<Integer> pList=new PriorityQueue<>();
+
+        pList.add(1);
+        pList.add(2);
+        pList.add(3);
+        pList.offer(4);
+
+        while(!pList.isEmpty()){
+            System.out.println(pList.poll());
+        }
+
+        finishTime=(int) System.nanoTime();
+        System.out.println(finishTime-startTime);
+
+        //Задача 4.5
+        StackList s1 = new StackList();
+        s1.push("Test");
+        s1.display(0);
+
+
 
     }
 
 
 }
+
